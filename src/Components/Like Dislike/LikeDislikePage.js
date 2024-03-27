4a7a89109350d2946564b399bb28a419ef26189b
Pritika04 <81809982+Pritika04@users.noreply.github.com>
2024-03-26 22:49:13 -0500

    Add files via upload

--- src/Components/Like Dislike/LikeDislikePage.js
+++ src/Components/Like Dislike/LikeDislikePage.js
@@ -1,6 +1,6 @@
 // In LikeDislikePage.js
 
-import React from 'react';
+import React, { useState } from 'react';
 import './LikeDislikePage.css';
 import profileImage from '../../Images/profile-image.jpg'; // Import the image file
 
@@ -10,6 +10,25 @@ const LikeDislikePage = ({ profileData }) => {
   // }
 
   // const { name, email, age, major, minor, interests, funFact, tags } = profileData;
+
+  const [showLikeNotification, setShowLikeNotification] = useState(false);
+  const [showDislikeNotification, setShowDislikeNotification] = useState(false);
+
+  const handleLike = () => {
+    setShowLikeNotification(true); // Show like notification
+  };
+
+  const handleDislike = () => {
+    setShowDislikeNotification(true); // Show dislike notification
+  };
+
+  const handleCloseLikeNotification = () => {
+    setShowLikeNotification(false); // Close like notification
+  };
+
+  const handleCloseDislikeNotification = () => {
+    setShowDislikeNotification(false); // Close dislike notification
+  };
 
   return (
     <div className="like-dislike-page">        
@@ -24,15 +43,29 @@ const LikeDislikePage = ({ profileData }) => {
           <p>Interests/Hobbies: {interests}</p>
           <p>Fun Fact: {funFact}</p>
           <p>Tags: {tags.join(', ')}</p> */}
-          <h2 className="profile-name">Profile Name</h2>
+          <h2 className="profile-name">Milo</h2>
         </div>
         <div className="actions">
-          <button className="action-button like">&#10003;</button>
-          <button className="action-button dislike">&#10007;</button>
+          <button className="action-button like" onClick={handleLike}>&#10003;</button>
+          <button className="action-button dislike" onClick={handleDislike}>&#10007;</button>
         </div>
       </div>
+      {showLikeNotification && (
+        <div className="notification" onClick={handleCloseLikeNotification}>
+          <p>New friend added! Click <a href="#">here</a> to start chatting :)</p>
+          <button className="close-button" onClick={handleCloseLikeNotification}>❌</button>
+        </div>
+      )}
+      {showDislikeNotification && (
+        <div className="notification" onClick={handleCloseDislikeNotification}>
+          <p>Profile disliked :/ Recommending new profile...</p>
+          <button className="close-button" onClick={handleCloseDislikeNotification}>❌</button>
+        </div>
+      )}
     </div>
   );
 };
 
 export default LikeDislikePage;
+
+
